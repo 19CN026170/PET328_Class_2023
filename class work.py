709d2dba8e01e87695cbfa0788188e033d6439b3
Joy Ogunpola <129372591+19CN026170@users.noreply.github.com>
2023-05-15 19:24:06 +0100

    Add files via upload

---
+++ class work.py
@@ -0,0 +1,25 @@
+Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========== RESTART: C:\Users\HP\Documents\PYTHON\creating a funtion.py =========
+>>> stoiip(40,0.23,15,0.18,1.21)
+725533.2892561984
+>>> 
+========== RESTART: C:\Users\HP\Documents\PYTHON\creating a funtion.py =========
+>>> stoiip(40,0.23,15,0.18,1.21)
+725533.29
+>>> type(2023)
+<class 'int'>
+>>> type(2.3)
+<class 'float'>
+>>> type(3.0)
+<class 'float'>
+>>> n=2
+>>> return n
+SyntaxError: 'return' outside function
+>>> n=3
+>>> return (n)
+SyntaxError: 'return' outside function
+>>> n=3
+>>> print (n)
+3
